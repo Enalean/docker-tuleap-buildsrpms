fc61ead7131ad35bb3acc3e540d29321ac37bc59
Martin Goyot <martin.goyot@enalean.com>
2014-10-30 10:26:15 +0100

    use the new makefile target including node

--- run.sh
+++ run.sh
@@ -5,9 +5,9 @@ mkdir -p /srpms/rhel5
 mkdir -p /srpms/rhel5-st
 mkdir -p /srpms/rhel6
 
-make -C $BASE_PATH/tools/rpm srpms OS="rhel6";
+make -C $BASE_PATH/tools/rpm srpms-docker OS="rhel6";
 mv /root/rpmbuild/SRPMS/* /srpms/rhel6;
-make -C $BASE_PATH/tools/rpm srpms OS="rhel5";
+make -C $BASE_PATH/tools/rpm srpms-docker OS="rhel5";
 mv /root/rpmbuild/SRPMS/* /srpms/rhel5;
-make -C $BASE_PATH/tools/rpm srpms OS="rhel5" PKG_NAME="codendi_st";
+make -C $BASE_PATH/tools/rpm srpms-docker OS="rhel5" PKG_NAME="codendi_st";
 mv /root/rpmbuild/SRPMS/* /srpms/rhel5-st;
