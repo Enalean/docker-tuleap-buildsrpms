9590fa81d51ded248654c5291ccaf328f5487d49
Manuel Vacelet <manuel.vacelet@enalean.com>
2014-11-14 10:39:53 +0100

    Allow to build a specific tag

--- run.sh
+++ run.sh
@@ -1,13 +1,30 @@
 #!/bin/bash
+
+#set -e
+
 BASE_PATH="/tuleap";
+MAKE_OPTIONS=""
+
+options=`getopt -o h -l git: -- "$@"`
+
+eval set -- "$options"
+while true
+do
+    case "$1" in
+    --git)
+        MAKE_OPTIONS="$MAKE_OPTIONS GIT_BRANCH=$2";
+        shift 2;;
+    --)
+        shift 1; break ;;
+    *)
+        break ;;
+    esac
+done
 
 mkdir -p /srpms/rhel5
-mkdir -p /srpms/rhel5-st
 mkdir -p /srpms/rhel6
 
-make -C $BASE_PATH/tools/rpm srpms-docker OS="rhel6";
-mv /root/rpmbuild/SRPMS/* /srpms/rhel6;
-make -C $BASE_PATH/tools/rpm srpms-docker OS="rhel5";
-mv /root/rpmbuild/SRPMS/* /srpms/rhel5;
-make -C $BASE_PATH/tools/rpm srpms-docker OS="rhel5" PKG_NAME="codendi_st";
-mv /root/rpmbuild/SRPMS/* /srpms/rhel5-st;
+make -C $BASE_PATH/tools/rpm srpms-docker OS="rhel6" $MAKE_OPTIONS
+mv /root/rpmbuild/SRPMS/* /srpms/rhel6
+make -C $BASE_PATH/tools/rpm srpms-docker OS="rhel5" $MAKE_OPTIONS
+mv /root/rpmbuild/SRPMS/* /srpms/rhel5
