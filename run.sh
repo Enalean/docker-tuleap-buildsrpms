256310bac4fe5b537a43be3d2bbfa90c378a38d0
Christian Bayle <christian.bayle@orange.com>
2014-11-17 19:58:34 +0100

    remove changes

--- run.sh
+++ run.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 BASE_PATH="/tuleap";
 
-#mkdir -p /srpms/rhel5
-#mkdir -p /srpms/rhel5-st
+mkdir -p /srpms/rhel5
+mkdir -p /srpms/rhel5-st
 mkdir -p /srpms/rhel6
 
 make -C $BASE_PATH/tools/rpm srpms-docker OS="rhel6";
 mv /root/rpmbuild/SRPMS/* /srpms/rhel6;
-#make -C $BASE_PATH/tools/rpm srpms-docker OS="rhel5";
-#mv /root/rpmbuild/SRPMS/* /srpms/rhel5;
-#make -C $BASE_PATH/tools/rpm srpms-docker OS="rhel5" PKG_NAME="codendi_st";
-#mv /root/rpmbuild/SRPMS/* /srpms/rhel5-st;
+make -C $BASE_PATH/tools/rpm srpms-docker OS="rhel5";
+mv /root/rpmbuild/SRPMS/* /srpms/rhel5;
+make -C $BASE_PATH/tools/rpm srpms-docker OS="rhel5" PKG_NAME="codendi_st";
+mv /root/rpmbuild/SRPMS/* /srpms/rhel5-st;
